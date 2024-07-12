3b2216435dc616d5b963b1e304779ae0615a5f5b
komarazer <peeranut_pp@live.com>
2024-07-12 17:21:52 +0700

    mahanakon update

--- lab/login.php
+++ lab/login.php
@@ -1,5 +1,6 @@
 <?php
-    echo "<h1> User: </h1>"
-    echo "<h1> Password: </h1>"
-    "TUB"
+    echo "<h1><table></h1>";
+    echo "<h1> User: </h1>";
+    echo "<h1> Password: </h1>";
+    echo "<h1>"</table></h1>";
     ?>
