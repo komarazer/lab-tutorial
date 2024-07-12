e4de8ad5de5e8b8d4ba2e23e1ab032571a12d9ef
komarazer <peeranut_pp@live.com>
2024-07-12 16:50:18 +0700

    TUK

---
+++ lab/index.php
@@ -0,0 +1,8 @@
+<?php 
+    echo "<h1>My repository</h1>"
+    echo "<h1>My Name is Ozen</h1>"    
+    echo "<h1>My Name is MaLawjaa</h1>"    
+    echo "<h1>My Name is PaiLawjaa</h1>"    
+    echo "<h1>My Name is Come</h1>"    
+    echo "<h1>My Name is Sun</h1>"    
+    ?>
